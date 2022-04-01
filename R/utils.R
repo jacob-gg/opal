58d64e28291a3199b6855a3d8e2710e40178b6b8
jacob-gg <jrg2xq@virginia.edu>
2022-04-01 11:40:53 -0400

    make mailto: prefix in email_gen() optional

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,8 @@
 # Unexported utility functions for `opal`
 # jacob-gg
 
-email_gen <- function() {
+email_gen <- function(mailto = T) {
   usr <- paste0(sample(c(letters, 1:9), sample(5:9, 1), replace = T), collapse = '')
   domain <- paste0(sample(c(letters, 1:9), sample(5:9, 1), replace = T), collapse = '')
-  out <- paste0('mailto:', usr, '@', domain, '.com')
-  out
+  paste0(ifelse(mailto == T, 'mailto:', ''), usr, '@', domain, '.com')
 }
